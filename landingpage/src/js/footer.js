3d1a8ef73594541dc959d16da14abc1f83e7e283
CaledTarique <caled.98@hotmail.com>
2025-07-27 14:40:47 -0300

    Remoção da pasta da landing page

--- landingpage/src/js/footer.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const footer = document.querySelector(".rodape-texto");
-    console.log(footer)
-    let date = new Date().getFullYear();
-
-    footer.innerHTML = `© ${date} Consat'prev. Todos os direitos reservados.`;
-});
